59fbe19e5ec92763a45e166337e790e1b2bc1808
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-06-10 17:15:20 -0700

    added a promise chaining example

---
+++ src/playground/promise.js
@@ -0,0 +1,11 @@
+require('./../db/mongoose')
+const User = require('./../models/user')
+
+User.findByIdAndUpdate('60c184719740833369d183ff', {age: 0}).then((user)=>{
+    console.log(user)
+    return User.countDocuments({age: 0})
+}).then((result)=>{
+    console.log(result)
+}).catch((e)=>{
+    console.log(e)
+})
