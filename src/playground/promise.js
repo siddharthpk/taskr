a2828501ace08fce781741afa8729e5999e36c6d
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-06-10 23:51:14 -0700

    using aync-await

--- src/playground/promise.js
+++ src/playground/promise.js
@@ -9,3 +9,15 @@ User.findByIdAndUpdate('60c184719740833369d183ff', {age: 0}).then((user)=>{
 }).catch((e)=>{
     console.log(e)
 })
+
+const updateAgeAndCount = async (id, age)=>{
+    const user = await User.findByIdAndUpdate(id, {age}) //Shorthand for age as we want default values
+    const count = await User.countDocuments({age})
+    return count
+}
+
+updateAgeAndCount('60c184719740833369d183ff', 23).then((count)=>{
+    console.log(count)
+}).catch((e)=>{
+    console.log(e)
+})
