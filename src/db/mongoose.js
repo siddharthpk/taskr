614ba6b4f4b0358888d5cdfefb068947815c7468
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-06-10 17:15:00 -0700

    added FindAndModify option

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 //Connect to DB 
 mongoose.connect('mongodb://127.0.0.1:27017/taskr-api'),{
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    userFindAndModify: false
 }
 
