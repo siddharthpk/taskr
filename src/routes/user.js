f0c9e5cb7cd84665ea5ed7947b1a735accf4a82a
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-06-14 23:07:24 -0700

    added middleware support & encryption

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { update } = require('../models/user')
 const User = require('../models/user')
 const router = new express.Router()
 
@@ -52,7 +53,14 @@ router.patch('/users/:id', async (req,res)=>{
     }
     
     try{
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators:true})
+        const user = await User.findById(req.params.id)
+        
+        toUpdate.forEach((update)=>user[update] = req.body[update])
+
+        await user.save()
+
+        // Removed the line below as findByIdAndUpdate bypasses middleware
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators:true})
 
         if(!user){
             return res.status(404).send()
