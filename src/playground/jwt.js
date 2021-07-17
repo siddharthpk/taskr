3a65eb0983370ae1230f5e573c7967fcac46ff71
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-07-16 20:05:40 -0700

    added token verification & expiration time

--- src/playground/jwt.js
+++ src/playground/jwt.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken')
 
 const jwtFunc = async ()=> {
-    var token = jwt.sign({_id: 'abc123' },'thisisthesecretstring')
+    var token = jwt.sign({_id: 'abc123' },'thisisthesecretstring', {expiresIn: '180 seconds'})
 
     console.log(token)
+
+    const data = jwt.verify(token, 'thisisthesecretstring')
+    console.log(data)
     
 }
 
