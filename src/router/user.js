2bbd3b7fcc79f2f8e44eccb54cd7ad4ca14e8ec1
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-09-13 20:54:52 -0700

    added email feature

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
 
 
@@ -10,6 +13,7 @@ router.post('/users', async (req,res) =>{
    
     try{
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.genAuthToken()
         res.status(201).send({user, token})
     } catch (e){
