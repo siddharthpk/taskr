d822b5c56e059a2d62b28de8c8bf6bbb2f6c67c8
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-09-25 00:30:14 -0700

    Added task owner feature at creation

--- src/router/task.js
+++ src/router/task.js
@@ -5,7 +5,10 @@ const router = new express.Router()
 
 // Task Creation 
 router.post('/tasks', auth, async (req,res)=>{
-    const task = new Task(req.body)
+    const task = new Task({
+        ...req.body, 
+        owner: req.user._id
+    })
 
     try{
         await task.save()
