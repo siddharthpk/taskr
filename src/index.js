7b6354f4b384332a1d821e53f03e7d577b54579d
Siddharth Pathak <siddharthpathak415@gmail.com>
2024-02-04 01:00:54 -0800

    file clean up

--- src/index.js
+++ src/index.js
@@ -8,13 +8,6 @@ require('./db/mongoose') // --> DO NOT DELETE
 const app = express()
 const port = process.env.PORT || 3000
 
-// Express Middleware for token authentication step
-// app.use((req, res, next)=>{
-//     //console.log(req.method, req.path)
-//     //next()
-//     res.status(503).send('Under Maintenance')
-// })
-
 // Accepting json
 app.use(express.json())
 
@@ -28,20 +21,3 @@ app.use(taskRouter)
 app.listen(port, ()=>{
     console.log('Server is running on port ' + port)
 })
-
-/*
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () =>{
-    const task = await Task.findById('614ecf563c4570873647749a')
-    await task.populate('owner').execPopulate() // populate finds the associated user with the owner id
-    //console.log(task.owner)
-
-    const  user = await User.findById('614eceaa94008c86686d7f84')
-    await user.populate('tasks').execPopulate()
-    //console.log(user.tasks)
-}   
-
-main()
-*/
