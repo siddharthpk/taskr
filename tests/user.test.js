bebc4ea89bdd776235f80ada23ae22ca7c032523
Siddharth Pathak <siddharthpathak415@gmail.com>
2024-02-13 23:31:59 -0800

    added userid & jwt tokens for test data

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,18 @@
 const request = require('supertest')
 const app = require('../src/app')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'Sid',
     email: 'sid@testexample.com',
-    password: 'f88rfrfrfrff'
+    password: 'f88rfrfrfrff',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
