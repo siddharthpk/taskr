770622a22d29fed5d6d831afaef3cadaa57e4dd8
Siddharth Pathak <siddharthpathak415@gmail.com>
2024-02-13 23:22:31 -0800

    added sample user for tests; Added Existing user login test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Sid',
+    email: 'sid@testexample.com',
+    password: 'f88rfrfrfrff'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 // Test User SignUp
 test('User SignUp should be successful', async ()=>{
@@ -8,4 +20,11 @@ test('User SignUp should be successful', async ()=>{
         email: 'siddharthpk@icloud.com',
         password: 'RERRFRgrgsrgs#@$'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
 })
