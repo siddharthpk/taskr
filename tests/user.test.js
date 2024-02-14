84a05fd82b603bf43fc1ba9ca28ed81a9fb59356
Siddharth Pathak <siddharthpathak415@gmail.com>
2024-02-14 00:05:53 -0800

    added avatar upload, user field update tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -96,3 +96,38 @@ test('Should not delete account for unauthenticate user', async () => {
         .send()
         .expect(401)
 })
+
+// User Avatar Upload 
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+// Valid User Field Update
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Sid Pathak'
+        })
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual('Sid Pathak')
+})
+
+// Invalid User Field Not Updated
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'Vancouver'
+        })
+        .expect(400)
+})
